f22e5a92d60a9fb10e50839a92b3d943e8971d56
matthiasVirat <virat.matthias@gmail.com>
2019-10-31 17:05:33 +0100

    En cours euregka

--- src/main/java/org/o7planning/locationVehicule/LocationVehiculeApplication.java
+++ src/main/java/org/o7planning/locationVehicule/LocationVehiculeApplication.java
@@ -2,9 +2,20 @@ package org.o7planning.locationVehicule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+//@EnableEurekaClient
 public class LocationVehiculeApplication {
+
+	@Bean
+//	@LoadBalanced
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LocationVehiculeApplication.class, args);
--- src/main/java/org/o7planning/locationVehicule/controller/ApiController.java
+++ src/main/java/org/o7planning/locationVehicule/controller/ApiController.java
@@ -2,6 +2,9 @@ package org.o7planning.locationVehicule.controller;
 
 import org.o7planning.locationVehicule.model.Vehicule;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
@@ -9,37 +12,37 @@ import java.util.Arrays;
 import java.util.List;
 
 
+//@RestController
 public class ApiController {
 
+
     @Autowired
-    private RestTemplate restTemplate = new RestTemplate();
+    private RestTemplate restTemplate;
 
-    public List<Vehicule> getVehicules() {
-
+    List<Vehicule> getVehicules() {
         Vehicule[] vehicules = restTemplate.getForObject("http://localhost:8081/vehiculeList", Vehicule[].class);
-
         return new ArrayList<>(Arrays.asList(vehicules));
     }
 
-    public List<Vehicule> getVehiculesOrdered(String critere, String ordre) {
-        Vehicule[] vehicules = restTemplate.getForObject("http://localhost:8081/vehiculeList/"+critere+"/"+ordre, Vehicule[].class);
+    List<Vehicule> getVehiculesOrdered(String critere, String ordre) {
+        Vehicule[] vehicules = restTemplate.getForObject("http://spring-boot-thymeleaf/vehiculeList/"+critere+"/"+ordre, Vehicule[].class);
         return new ArrayList<>(Arrays.asList(vehicules));
     }
 
-    public Vehicule getVehiculeById(int id){
-        return restTemplate.getForObject("http://localhost:8081/vehicule/"+id, Vehicule.class);
+    Vehicule getVehiculeById(int id){
+        return restTemplate.getForObject("http://spring-boot-thymeleaf/vehicule/"+id, Vehicule.class);
     }
 
-    public Vehicule postVehicule(Vehicule vehicule){
-        return restTemplate.postForObject("http://localhost:8081/addNew", vehicule, Vehicule.class);
+    Vehicule postVehicule(Vehicule vehicule){
+        return restTemplate.postForObject("http://spring-boot-thymeleaf/addNew", vehicule, Vehicule.class);
     }
 
-    public void deleteVehicule(int id) {
-        restTemplate.delete("http://localhost:8081/deleteVehicule/"+id);
+    void deleteVehicule(int id) {
+        restTemplate.delete("http://spring-boot-thymeleaf/deleteVehicule/"+id);
     }
 
-    public void updateVehicule(Vehicule vehicule) {
-        restTemplate.put("http://localhost:8081/updateVehicule", vehicule);
+    void updateVehicule(Vehicule vehicule) {
+        restTemplate.put("http://spring-boot-thymeleaf/updateVehicule", vehicule);
     }
 
 }
